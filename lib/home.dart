a8717d10855e929cd2950ffb65b544611c8c1ca2
Mariia1908 <akulich.m@edu.narfu.ru>
2022-03-17 22:39:00 +0300

    kontrol'naya

---
+++ lib/home.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:pr/screens/explore_screen.dart';
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  int _selectedIndex = 0;
+
+  static List<Widget> pages = <Widget>[
+    ExploreScreen(),
+    Container(color: Colors.white),
+    // TODO: Replace with RecipesScreen
+    Container(color: Colors.green),
+    Container(color: Colors.blue),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Книга рецептов',
+          style: Theme.of(context).textTheme.headline6,
+        ),
+      ),
+      body: pages[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        selectedItemColor: Theme.of(context).textSelectionTheme.selectionColor,
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+        items: <BottomNavigationBarItem>[
+          const BottomNavigationBarItem(
+            icon: Icon(Icons.explore),
+            label: 'Поиск',
+          ),
+          const BottomNavigationBarItem(
+            icon: Icon(Icons.book),
+            label: 'Рецепты',
+          ),
+          const BottomNavigationBarItem(
+            icon: Icon(Icons.list),
+            label: 'Купить',
+          ),
+        ],
+      ),
+    );
+  }
+}
